6160787b9843dc06be348a5861f91596ed446db3
Matt Smith <psykzz@users.noreply.github.com>
2017-03-19 17:50:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ var GUILD_ID = process.env.GUILD_ID || '';
 
 var api = new GW2Api(API_KEY);
 
+var onError = (bot, chatId, message) => {
+  bot.sendMessage(chatId, message, {
+    "parse_mode": "Markdown",
+    "disable_web_page_preview": true
+  });
+}
+
 function getDailyFractals(callback) {
   var match = "Daily Tier 4"
   api.getAchievementsByCategory(88, (err, res) => {
+      if(err) {
+        callback(err)
+      }
+    
       var promises = res.achievements.map((achiev) => {
           return new Promise((resolve, reject) => {
               api.getAchievementDetails(achiev, (err, detail) => {
@@ -49,6 +60,10 @@ bot.on('message', (msg) => {
   var chatId = msg.chat.id;
   if (msg.text.match(/\!motd/)) {
     api.getMotd(GUILD_ID, (err, res) => {
+       if(err) {
+        onError(bot, chatId, "Error getting motd. Is the GW2 API Down?")
+       }
+      
       bot.sendMessage(chatId, res.motd, {
         "parse_mode": "Markdown",
         "disable_web_page_preview": true
@@ -58,6 +73,10 @@ bot.on('message', (msg) => {
   
   if(msg.text.match(/\!fractals/)) {
     getDailyFractals((err, results) => {
+      if(err) {
+        onError(bot, chatId, "Error getting daily fractals. Is the GW2 API Down?")
+      }
+      
       var output = "Today's daily fractals: \n```\n"
 
       results.forEach(res => {
